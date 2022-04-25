d146d4316110036bff04b778b13c84ae37665069
Adarsh kumar <adarsh2350i@gmail.com>
2022-04-25 05:58:35 +0530

    Update trapezoidal.m

--- trapezoidal/trapezoidal.m
+++ trapezoidal/trapezoidal.m
@@ -1,7 +1,7 @@
 f=input('Enter function: ');
-h=input('Enter h: ')
-a=input('Enter a: ')
-b=input('Enter b: ')
+h=input('Enter h: ');
+a=input('Enter a: ');
+b=input('Enter b: ');
 I=h/2;
 sum=f(a)+f(b);
 a=a+h;
@@ -9,4 +9,4 @@ while a<b
     sum=sum+(2*f(a));
     a=a+h;
 end
-Ans=I*sum;
+Ans=I*sum
