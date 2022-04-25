36067e35864f3da941d791469c163a0ed908e6e2
Adarsh kumar <adarsh2350i@gmail.com>
2022-04-25 05:53:26 +0530

    Add files via upload

---
+++ trapezoidal/trapezoidal.m
@@ -0,0 +1,12 @@
+    f=input('Enter function: ');
+    h=input('Enter h: ')
+    a=input('Enter a: ')
+    b=input('Enter b: ')
+    I=h/2;
+    sum=f(a)+f(b);
+    a=a+h;
+    while a<b
+        sum=sum+(2*f(a));
+        a=a+h;
+    end
+    Ans=I*sum;
