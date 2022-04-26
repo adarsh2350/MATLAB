1bea846848ee49a24e5bccfa34df722798c3a503
Adarsh kumar <adarsh2350i@gmail.com>
2022-04-26 19:22:44 +0530

    Update runge_kutta_4thO.m

--- runge_kutta_4thO/runge_kutta_4thO.m
+++ runge_kutta_4thO/runge_kutta_4thO.m
@@ -1,1 +1,19 @@
-
+f=input('Enter function:');
+x0=input('Enter x0: ');
+y0=input('Enter y0: ');
+h=input('Enter h: ');
+X=input('Enter required X: ');
+x=x0;
+while x<X
+    x=x+h;
+    k1=h*f(x0,y0);
+    k2=h*f(x0+(h/2),y0+(k1/2));
+    k3=h*f(x0+(h/2),y0+(k2/2));
+    k4=h*f(x0+h,y0+k3);
+    K=(1/6)*(k1+k4 + 2*(k2+k3));
+    y=y0 + K;
+    Y=y;
+    y0=y;
+    x0=x;
+end
+Y
