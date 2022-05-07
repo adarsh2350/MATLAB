d7993d36bd6ff4f681312ec2773512c829151d11
Adarsh kumar <adarsh2350i@gmail.com>
2022-05-07 21:40:41 +0530

    Create RK_Fourth_200107003.m

---
+++ runge_kutta_4thO/RK_Fourth_200107003.m
@@ -0,0 +1,26 @@
+%%RK 4th order
+function [Y] = RK_Fourth_200107003(fxy, step, a, b, yi)
+
+format shortG;
+
+n=(b-a)/step+1;
+result=zeros(n,1);
+
+xi=a;
+
+Y=zeros(n,1);
+%fprintf('%d is the final answer from Rk 4th order method\n',Y(n))
+
+
+for i=1:n-1
+    Y(i)=yi;
+    result(i)=fxy(xi,yi);
+    K_1=result(i);
+    K_2=fxy(xi+step/2,yi+K_1/2);
+    K_3=fxy(xi+step/2,yi+K_2/2);
+    K_4=fxy(xi+step,yi+K_3);
+    Y(i+1)=yi+(1/6)*(K_1+2*K_2+2*K_3+K_4)*step;
+    xi=xi+step;
+    yi=Y(i+1);
+end
+    
